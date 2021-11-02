c51d1b7938f9797839c3fb4f0e059963ee2afe4c
stentibbing <sten.tibbing@gmail.com>
2021-11-02 14:18:23 +0200

    Added Git Updater support

--- nm_scroller.php
+++ nm_scroller.php
@@ -16,6 +16,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       nm_scroller
  * Domain Path:       /languages
+ * GitHub Plugin URI: stentibbing/nm_scroller
  */
 
 // If this file is called directly, abort.
