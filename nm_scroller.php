853c535c872ab48b6c124ece45f551734888e69b
stentibbing <sten.tibbing@gmail.com>
2021-11-03 18:23:44 +0200

    Updated version number

--- nm_scroller.php
+++ nm_scroller.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Nordic Milk Scroller
  * Plugin URI:        https://www.taifuun.ee
  * Description:       This plugin adds fading scroller with pagination.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Taifuun
  * Author URI:        https://www.taifuun.ee
  * License:           GPL-2.0+
@@ -27,7 +27,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Currently plugin version.
  */
-define( 'NM_SCROLLER_VERSION', '1.0.2' );
+define( 'NM_SCROLLER_VERSION', '1.0.3' );
 
 /**
  * The code that runs during plugin activation.
