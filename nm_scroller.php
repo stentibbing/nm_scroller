4be45752c7ac5ec47c7f6ab23f7a1639474b5cc9
stentibbing <sten.tibbing@gmail.com>
2022-02-02 15:34:14 +0200

    Updated version number

--- nm_scroller.php
+++ nm_scroller.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Nordic Milk Scroller
  * Plugin URI:        https://www.taifuun.ee
  * Description:       This plugin adds scroller with pagination
- * Version:           1.0.7
+ * Version:           1.1.0
  * Author:            Taifuun OÜ
  * Author URI:        https://www.taifuun.ee
  * License:           GPL-2.0+
@@ -27,7 +27,7 @@ if (! defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-define('NM_SCROLLER_VERSION', '1.0.7');
+define('NM_SCROLLER_VERSION', '1.1.0');
 
 /**
  * The code that runs during plugin activation.
